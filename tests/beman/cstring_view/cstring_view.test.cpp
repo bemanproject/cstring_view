3a697e3fc5e40e153781a83fd2a0ddbaa6876736
Dascandy <dascandy@gmail.com>
2025-08-22 10:28:08 +0200

    fix minor coding style issue

--- tests/beman/cstring_view/cstring_view.test.cpp
+++ tests/beman/cstring_view/cstring_view.test.cpp
@@ -7,7 +7,7 @@ using namespace beman::literals;
 using namespace std::literals;
 
 TEST(StringView, ConstructionDestruction) {
-    std::string s = "hello";
+    std::string         s = "hello";
     beman::cstring_view h1 = "hello";
     beman::cstring_view h2 = h1;
 
@@ -22,7 +22,7 @@ TEST(StringView, ConstructionDestruction) {
     EXPECT_EQ(h1 <=> h1, std::strong_ordering::equal);
     EXPECT_EQ(h1[0], 'h');
     auto first = h1.substr(0, 2);
-    auto end = h1.substr(2);
+    auto end   = h1.substr(2);
     EXPECT_TRUE((std::is_same_v<decltype(first), std::string_view>));
     EXPECT_TRUE((std::is_same_v<decltype(end), beman::cstring_view>));
     EXPECT_EQ(first, "he");
